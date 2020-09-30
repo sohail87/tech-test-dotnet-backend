d5782cadf99cf0a543c3eb1d156db82ce29b9e2a
sohail87 <sohaildeen73@gmail.com>
2020-10-01 00:03:54 +0100

    moved request class to its own file

---
+++ Moonpig.PostOffice.Core/DespatchDateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Moonpig.PostOffice.Core
+{
+    public class DespatchDateRequest
+    {
+        public List<int> ProductIds { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public DespatchDateRequest(List<int> productIds, DateTime orderDate)
+        {
+            ProductIds = productIds;
+            OrderDate = orderDate;
+        }
+
+        public DespatchDateRequest() { }
+    }
+}
