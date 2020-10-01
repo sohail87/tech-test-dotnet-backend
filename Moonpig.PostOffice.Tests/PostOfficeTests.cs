4602b3674df4dd318daa2f92e37d94bada6cc1fc
sohail87 <sohaildeen73@gmail.com>
2020-10-01 03:02:03 +0100

    renamed tests to identify what is missing

--- Moonpig.PostOffice.Tests/PostOfficeTests.cs
+++ Moonpig.PostOffice.Tests/PostOfficeTests.cs
@@ -1,16 +1,15 @@
-﻿using Moonpig.PostOffice.Core;
+﻿using System;
+using System.Collections.Generic;
+using Moonpig.PostOffice.Api.Controllers;
+using Moonpig.PostOffice.Core;
+using Shouldly;
+using Xunit;
 
 namespace Moonpig.PostOffice.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using Api.Controllers;
-    using Shouldly;
-    using Xunit;
-
     public class PostOfficeTests
     {
-        private DespatchDateController _controller;
+        private readonly DespatchDateController _controller;
 
         public PostOfficeTests()
         {
@@ -19,12 +18,12 @@ namespace Moonpig.PostOffice.Tests
 
         public class SingleProductLeadTimeIsAddedToDespatchDate : PostOfficeTests
         {
-            private readonly DateTime _mondayOrderDate;
-
             public SingleProductLeadTimeIsAddedToDespatchDate()
             {
                 _mondayOrderDate = new DateTime(2020, 9, 28);
             }
+
+            private readonly DateTime _mondayOrderDate;
 
             [Fact]
             public void MondayOrder_WithLeadTimeOfOneDay_HasDespathDateOfTuesday()
@@ -34,66 +33,47 @@ namespace Moonpig.PostOffice.Tests
             }
 
             [Fact]
-            public void MondayOrder_WithLeadTimeOfTwoDays_HasDespathDateOfWednesday()
-            {
-                var date = _controller.Get(DespatchDateRequest.SingleProduct(2, _mondayOrderDate));
-                date.Date.Date.ShouldBe(_mondayOrderDate.Date.AddDays(2));
-            }
-
-            [Fact]
             public void MondayOrder_WithLeadTimeOfThreeDays_HasDespathDateOfThursday()
             {
                 var date = _controller.Get(DespatchDateRequest.SingleProduct(3, _mondayOrderDate));
                 date.Date.Date.ShouldBe(_mondayOrderDate.Date.AddDays(3));
             }
+
+            [Fact]
+            public void MondayOrder_WithLeadTimeOfTwoDays_HasDespathDateOfWednesday()
+            {
+                var date = _controller.Get(DespatchDateRequest.SingleProduct(2, _mondayOrderDate));
+                date.Date.Date.ShouldBe(_mondayOrderDate.Date.AddDays(2));
+            }
         }
 
         public class SupplierWithLongestLeadTimeIsUsedForCalculation : PostOfficeTests
         {
-
             [Fact]
             public void MondayOrder_WithMaxLeadTimeOfTwoDays_HasDespathDateOfWednesday()
             {
                 var mondayOrderDate = new DateTime(2020, 9, 28);
-                var date = _controller.Get(new DespatchDateRequest(new List<int>{1, 2}, mondayOrderDate));
+                var date = _controller.Get(new DespatchDateRequest(new List<int> {1, 2}, mondayOrderDate));
                 date.Date.Date.ShouldBe(mondayOrderDate.Date.AddDays(2));
             }
-
         }
 
-        public class DespatchDateIncorporatesWeekendClosure : PostOfficeTests
-        {
-        [Fact]
-        public void SaturdayHasExtraTwoDays()
-        {
-            var orderDate = new DateTime(2018,1,26);
-            var date = _controller.Get(DespatchDateRequest.SingleProduct(1, orderDate));
-            date.Date.ShouldBe(orderDate.Date.AddDays(3));
-        }
-
-        [Fact]
-        public void SundayHasExtraDay()
-        {
-            var orderDate = new DateTime(2018, 1, 25);
-            var date = _controller.Get(DespatchDateRequest.SingleProduct(3, orderDate));
-            date.Date.ShouldBe(orderDate.Date.AddDays(5));
-        }
-}
-        public class SupplierLeadTimeIncorporatesWeekendClosure : PostOfficeTests
+        public class LeadTimeIsNotCountedOverAWeekend : PostOfficeTests
         {
             [Fact]
-            public void FridayOrderWithMaxLeadTimeOfTwoDaysWillBeFourDays()
-            {
-                var orderDate = new DateTime(2020, 9, 25);
-                var date = _controller.Get(DespatchDateRequest.SingleProduct(2, orderDate));
-                date.Date.ShouldBe(orderDate.Date.AddDays(4));
-            }
-            [Fact]
-            public void ThursdayOrderWithMaxLeadTimeOfThreeDaysWillBeFiveDays()
+            public void ThursdayOrder_WithLeadTimeOfThreeDay_HasDespatchDateOfTuesday()
             {
                 var orderDate = new DateTime(2020, 10, 1);
                 var date = _controller.Get(DespatchDateRequest.SingleProduct(3, orderDate));
                 date.Date.ShouldBe(orderDate.Date.AddDays(5));
+            }
+
+            [Fact]
+            public void FridayOrder_WithLeadTimeOfOneDay_HasDespatchDateOfMonday()
+            {
+                var orderDate = new DateTime(2020, 10, 2);
+                var date = _controller.Get(DespatchDateRequest.SingleProduct(1, orderDate));
+                date.Date.ShouldBe(orderDate.Date.AddDays(3));
             }
         }
     }
